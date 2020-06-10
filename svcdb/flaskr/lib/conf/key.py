4846160074bb3fa3150fc86ab5756020e4d5d596
yangya <979734716@qq.com>
2020-06-10 12:12:02 +0900

    第一次提交

--- svcdb/flaskr/lib/conf/key.py
+++
@@ -1,2 +0,0 @@
-SECRET_KEY = "g8wKf0pvje6m2unhst3j"
-SALT = "762ifes9fj5wxjkeu0dk"
--- svcdb/flaskr/lib/svcdb_lib/arr_util.py
+++
@@ -1,7 +0,0 @@
-class ArrUtil:
-
-    def search_array(arr_ref, search_data):
-        if search_data not in arr_ref:
-            return -1
-
-        return arr_ref.index(search_data)
