889360e40fc3792beff35bd2f1bfb1f40f1870ed
yangya <979734716@qq.com>
2020-07-06 16:53:33 +0900

    修复bug

--- svcdb/flaskr/forms/outage_form.py
+++ svcdb/flaskr/forms/outage_form.py
@@ -4,6 +4,18 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, BooleanField, ValidationError, IntegerField
 
 
+def get_date_start():
+    date = datetime.now()
+    year = date.year - 1 if date.month < 4 else date.year
+    return str(year)
+
+
+def get_date_end():
+    date = datetime.now()
+    year = date.year - 1 if date.month < 4 else date.year
+    return str(year + 5)
+
+
 class OutageForm(FlaskForm):
     class Meta:
         csrf = False
@@ -18,16 +30,14 @@ class OutageForm(FlaskForm):
     c3 = BooleanField('Mechanical', default=True)
     c4 = BooleanField('Electrical', default=True)
     search = SubmitField("Search")
-    date_start = SelectField("Period")
-    date_end = SelectField("")
+    date_start = SelectField("Period", default=get_date_start)
+    date_end = SelectField("", default=get_date_end)
     page = IntegerField(default=1)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.date_start.choices = [(i, i) for i in map(str, range(datetime.now().year - 1, datetime.now().year + 21))]
-        self.date_start.default = str(datetime.now().year)
         self.date_end.choices = [(i, i) for i in map(str, range(datetime.now().year + 4, datetime.now().year + 26))]
-        self.date_end.default = str(datetime.now().year + 5)
 
     def validate_date_start(self, field):
         if field.data > self.date_end.data:
